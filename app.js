ce0bcbdf9622fcfbb18f6df949e0fa54202473a2
richardhartleydev <richardhartleydev@gmail.com>
2023-10-18 17:01:10 +0100

    planning

---
+++ app.js
@@ -0,0 +1,14 @@
+const options = {
+  strings: [
+    "^500 Father of 4 cats. 🐈",
+    "Problem Solver. 🧠",
+    "Ceraunophile. ⛈️",
+    "Fan of Sundays. ☀️",
+    "Video game connoisseur. 🎮",
+  ],
+  typeSpeed: 100,
+  backSpeed: 80,
+  loop: true,
+};
+
+const typed = new Typed(".text-animation", options);
